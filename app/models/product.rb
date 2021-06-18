fbe0977387f6c10efb331ffe10c0ffcb02ec673c
ashley <agahagan1@gmail.com>
2021-06-18 13:26:39 -0400

    adds associations and validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+  has_many :product_images
+
+  validates :title, :description, :condition, :price, :category, :trade, presence:true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :products
+
+  validates :first_name, :last_name, :zipcode, presence:true
+  validates :email, :username, uniqueness: true
 end
